0731d174f5a35a0142a4685df7c068acb6ffefaf
sawers014 <74411308+sawers014@users.noreply.github.com>
2022-12-14 11:26:37 +0100

    Tic Tac Toe in node js

---
+++ TicTacToe.js
@@ -0,0 +1,73 @@
+/* -1 no one won, and there are still empty spaces
+ 1 'O' have won
+ 2 'O' have won
+ 0 there are no free square and no one one, so it's a draw 
+     TODO make the is_solved to work with char and not let, and make the board be full of chars instead of numbers
+*/
+function is_solved(board) {
+    let answer= 0
+    let empty = 0
+        if (board[1][1] == 1 && board[0][0] == 1 && board [2][2] == 1 || board[1][1] == 1 && board[0][2] == 1 && board[2][0] == 1)return 1;
+        if (board[1][1] == 2 && board[0][0] == 2 && board[2][2] == 2 || board[1][1] == 2 && board[0][2] == 2 && board[2][0] == 2) return 2;
+
+        for(let x=0; x < 3; x++){
+            if (board[x][0] == 1 && board[x][1] == 1 && board[x][2] == 1) return 1;
+            if (board[x][0] == 2 && board[x][1] == 2 && board[x][2] == 2) return 2;
+            if (board[0][x] == 1 && board[1][x] == 1 && board[2][x] == 1) return 1;
+            if (board[0][x] == 2 && board[1][x] == 2 && board[2][x] == 2)return 2;
+             
+        for(let y=0; y < 3; y++)   
+    
+            if (board[x][y] == 0) empty += 1;
+    }
+        if (empty > 0) answer =-1;
+    return answer;
+    }
+    
+    let board = new Array(3); // first array equivalent to rows 
+        for(i=0; i<board.length; i++) {   // inner array equivalent to columns
+            board[i] = new Array(3);
+        }
+    board =[
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+        ]
+    let row, column, choice;
+
+        const prompt = require('prompt-sync')();
+        choice = prompt("write 'x' if you'd like the 'x' to start or type 'o' to make the 'o' start ");
+        choice.toLocaleLowerCase()   // so it works even if the user input the choice in UPPERCASE
+           
+    for (let i = 0; i < 9; i++) {
+  
+    board.forEach(v=>console.log(...v)); //print the board
+        
+        row = prompt("choose a row 1-3 ");
+        row=Number(row)   //convert to integer, because by default the input is treated as string
+        column= prompt("choose a column 1-3 ");;
+        column=Number(column)
+        if (choice == 'x') {
+            board[row - 1][column - 1] = 1;
+            choice = 'o';
+        }
+        else {
+            board[row - 1][column - 1] = 2;
+            choice = 'x';
+        }
+        switch (is_solved(board)) {
+            case 1:
+                console.log("The 'x' have won ")
+                return 0;
+                break;
+
+            case 2:
+                console.log("The 'o' have won")
+                return 0;
+                break;
+            case 0:
+                console.log("it's a draw")
+                return 0;
+                break;
+        }
+    }
